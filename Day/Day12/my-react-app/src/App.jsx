3519c208686d1081031f1ebba119f3178439f0d9
yogita008 <yogita8073@gmail.com>
2026-08-18 21:46:10 +0530

    component no.3

--- Day/Day12/my-react-app/src/App.jsx
+++ Day/Day12/my-react-app/src/App.jsx
@@ -1,13 +1,13 @@
 import {Welcome} from "./components/P1.jsx";
-import Greet  from "./components/P2.jsx";
-function App() {
-  return (
+import Greet from "./components/P2.jsx";
+import {  MyMovies } from "./components/P3.jsx";
+function App(){
+  return(
     <div>
-    <Welcome/>
-    <Greet/>
+      <Welcome/>
+      <Greet/>
+      <MyMovies/>
     </div>
-    
   );
 }
-
-export default App
+export default App;
---
+++ Day/Day12/my-react-app/src/components/P3.jsx
@@ -0,0 +1,11 @@
+export function MyMovies() {
+  return (
+    <>
+      <ul>
+        <li>Movie 1</li>
+        <li>Movie 2</li>
+        <li>Movie 3</li>
+      </ul>
+    </>
+  );
+}
