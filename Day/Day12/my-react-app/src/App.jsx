c41285a396e972caf9e0b003d88018339c5437db
yogita008 <yogita8073@gmail.com>
2026-08-18 21:34:15 +0530

    function function| export default

--- Day/Day12/my-react-app/src/App.jsx
+++ Day/Day12/my-react-app/src/App.jsx
@@ -1,8 +1,10 @@
 import {Welcome} from "./components/P1.jsx";
+import Greet  from "./components/P2.jsx";
 function App() {
   return (
     <div>
     <Welcome/>
+    <Greet/>
     </div>
     
   );
---
+++ Day/Day12/my-react-app/src/components/P2.jsx
@@ -0,0 +1,8 @@
+export default function Greet() {
+    return(
+        <>
+            <h6>Hello my name is Yogita</h6>
+            <p>I'm learning React JS now.</p>
+        </>
+    )
+}
