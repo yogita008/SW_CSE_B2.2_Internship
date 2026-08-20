3506b0fe7a03a55e12e23f2faf1db6fb95f96b13
yogita008 <yogita8073@gmail.com>
2026-08-21 01:15:47 +0530

    list and keys with conditional rendering

--- Day/Day14/Day-14-react-app/src/components/LitsAndKeysOne.jsx
+++ Day/Day14/Day-14-react-app/src/components/LitsAndKeysOne.jsx
@@ -1,53 +1,44 @@
+
 function SimpleList() {
   const fruits = [
     // Only unique values
-    "apple",
-    "banana",
-    "mango",
-    "orange",
-    "grape",
-    "strawberry",
-    "blueberry",
-    "pineapple",
-    "watermelon",
-    "kiwi",
-    "peach",
-    "cherry",
-    "avocado",
-    "pomegranate",
-    "lemon",
+    // Un-comment following line to render the list
+    //"apple", "banana", "mango", "orange", "grape", "strawberry", "blueberry", "pineapple", "watermelon", "kiwi", "peach", "cherry", "avocado", "pomegranate", "lemon"
   ];
- const vegetablesWithId = [
+  const vegetablesWithId = [
     // Repeated/ duplicate names but with unique id
-  { "id": 1, "name": "carrot" },
-  { "id": 2, "name": "broccoli" },
-  { "id": 3, "name": "spinach" },
-  { "id": 4, "name": "tomato" },
-  { "id": 5, "name": "potato" },
-  { "id": 6, "name": "onion" },
-  { "id": 7, "name": "cucumber" },
-  { "id": 8, "name": "bell pepper" },
-  { "id": 9, "name": "garlic" },
-  { "id": 10, "name": "ginger" },
-  { "id": 11, "name": "lettuce" },
-  { "id": 12, "name": "cabbage" },
-  { "id": 13, "name": "mushroom" },
-  { "id": 14, "name": "cauliflower" },
-  { "id": 15, "name": "sweet potato" },
-  { "id": 16, "name": "zucchini" },
-  { "id": 17, "name": "zucchini" }, //duplicate
-];
-
+    { id: 1, name: "carrot" },
+    { id: 2, name: "broccoli" },
+    { id: 3, name: "spinach" },
+    { id: 4, name: "tomato" },
+    { id: 5, name: "potato" },
+    { id: 6, name: "onion" },
+    { id: 7, name: "cucumber" },
+    { id: 8, name: "bell pepper" },
+    { id: 9, name: "garlic" },
+    { id: 10, name: "ginger" },
+    { id: 11, name: "lettuce" },
+    { id: 12, name: "cabbage" },
+    { id: 13, name: "mushroom" },
+    { id: 14, name: "cauliflower" },
+    { id: 15, name: "sweet potato" },
+    { id: 16, name: "zucchini" },
+    { id: 17, name: "zucchini" }, //duplicate
+  ];
 
   return (
     <div>
       <h5>Fruit list</h5>
+      {fruits.length === 0 ? (<p>No Fruits available to display</p>
+      ):(
       <ul>
         {fruits.map((fruit) => (
           <li key={fruit}>{fruit}</li>
         ))}
       </ul>
 
+      )}
+      
       <h5>Vegetables list</h5>
       <ul>
         {vegetablesWithId.map((vegetables) => (
